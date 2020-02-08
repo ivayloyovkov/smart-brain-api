88c4ea0df4b11da2947dc7516f9472ac9980b257
ivayloyovkov <ivayloyovkovofficial@gmail.com>
2020-02-08 17:00:08 +0200

    fixing issues

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT () => {
+app.listen(process.env.PORT, () => {
 
 });
