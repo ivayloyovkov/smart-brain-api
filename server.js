368542ebe35d5b3f097982833b6557d8f06483dc
ivayloyovkov <ivayloyovkovofficial@gmail.com>
2020-02-08 07:29:40 +0200

    fixing issues

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const profile = require('./controllers/profile');
 
 const db = knex({
     client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: false
-    }
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 app.use(express.json());
