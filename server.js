3bbfbd73ca39d1af9231800aae8d0b0de0a75310
ivayloyovkov <ivayloyovkovofficial@gmail.com>
2020-02-10 10:12:07 +0200

    added validation

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors());
 
 app.get('/', (req, res) => {res.json(db.users)})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, bcrypt, db) })
-app.post('/register', [check('email').isEmail(), check('password').isLength({ min : 6})], (req, res) => { register.handleRegister(req, res, bcrypt, db) })
+app.post('/register', [check('email').isEmail(), check('password').isLength({ min : 6})], (req, res) => { register.handleRegister(req, res, bcrypt, db, check, validationResult) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
