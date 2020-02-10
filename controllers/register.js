b1523820dcbf38c63dc0c0c72999ae084a56c57b
ivayloyovkov <ivayloyovkovofficial@gmail.com>
2020-02-10 17:14:47 +0200

    added validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,40 +1,36 @@
-const handleRegister = (req, res, bcrypt, db) => {
-    const { check, validationResult } = require('express-validator');
+const handleRegister = (req, res, bcrypt, db, check, validationResult) => {
+    // const { check, validationResult } = require('express-validator');
     const { email, name, password } = req.body;
-    if (!check('email').isEmail() && !check('password').isLength({ min: 5 })) {
-        return res.status(400).json('Bad')
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
     }
-    else {
-        // const errors = validationResult(validationReq);
-        // if (!errors.isEmpty()) {
-        //     return res.status(422).json({ errors: errors.array() });
-        // }
 
-        const hash = bcrypt.hashSync(password, 8);
-        db.transaction(trx => {
-            trx.insert({
-                    hashedpass: hash,
-                    email: email
-                })
-                .into('login')
-                .returning('email')
-                .then(loginEmail => {
-                    return trx('users')
-                        .returning('*')
-                        .insert({
-                            email: loginEmail[0],
-                            name: name,
-                            joined: new Date()
-                        })
-                        .then(user => {
-                            res.json(user[0]);
-                        })
-                        .then(trx.commit)
-                        .catch(trx.callback)
-                })
-                .catch(err => res.status(400).json('Unable to register'));
-        })
-    }
+    const hash = bcrypt.hashSync(password, 8);
+    db.transaction(trx => {
+        trx.insert({
+                hashedpass: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .then(trx.commit)
+                    .catch(trx.callback)
+            })
+            .catch(err => res.status(400).json('Unable to register'));
+    })
+}
 }
 module.exports = {
     handleRegister
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const app = express();
 const knex = require('knex');
-
+const { check, validationResult } = require('express-validator');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.get('/', (req, res) => { res.json(db.users) })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, bcrypt, db) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, bcrypt, db) })
+app.post('/register', [check('email').isEmail(), check('password').isLength({ min: 5 })], (req, res) => { register.handleRegister(req, res, bcrypt, db, check, validationResult) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
